7a2eb33729e6b553168b08d77472b034416b4dba
shaw <haseeer@gmail.com>
2021-06-16 20:30:40 +0800

    超大弹幕处理方式可配置 close #1

--- src/bigDanmaku.js
+++ src/bigDanmaku.js
@@ -1,38 +1,56 @@
 // ==UserScript==
 // @name         acfun超大弹幕缩小
-// @version      0.1.0
+// @version      0.2.0
 // @description  超大弹幕很影响观看体验，干掉它吧！
 // @author       泥壕
 // @match        https://www.acfun.cn/v/*
 // @namespace https://greasyfork.org/users/50175
 // ==/UserScript==
- 
+
 (function () {
   'use strict';
+  const log = arg => console.log('%c 超大弹幕缩小 ', 'background:#03a9f4;color:white', arg);
+  let wentDel = Boolean(localStorage.getItem('超大弹幕处理方式'))
+  log(`欢迎使用~  当前配置：超大弹幕 ${wentDel ? '删除' : '缩小'}`)
+  log(`运行 bigDanmakuDel(true) 删除超大弹幕，运行 bigDanmakuDel(false) 缩小超大弹幕。`)
   let timeId = null
   let count = 0
   const reduceDanmaku = () => {
     timeId = setInterval(() => {
       const dom = document.querySelectorAll('.danmaku-item.danmaku-item-move')
       Array.prototype.forEach.call(dom, e => {
-        if (e.style.fontSize !== '25px') {
+        if (parseInt(e.style.fontSize) > 25) {
           count++
           window.requestAnimationFrame(() => {
             log(e.innerText)
-            e.style.fontSize = '25px'
+            if (wentDel) {
+              e.remove()
+            } else {
+              e.style.fontSize = '25px'
+            }
           })
         }
       })
     }, 600);
   }
-  const log = arg => console.log('%c 超大弹幕缩小 ', 'background:#03a9f4;color:white', arg);
   const videoElement = document.querySelector('video')
+
   videoElement.addEventListener('play', () => {
     log('=开始=');
     reduceDanmaku()
   })
   videoElement.addEventListener('pause', () => {
     window.clearInterval(timeId)
-    log(`=中止=  已处理${count}条超大弹幕`);
+    log(`=中止=  共处理${count}条超大弹幕`);
   })
+
+  window.bigDanmakuDel = action => {
+    wentDel = action
+    log(`配置修改：超大弹幕 ${wentDel ? '删除' : '缩小'}`)
+    if (action) {
+      localStorage.setItem('超大弹幕处理方式', 1)
+    } else {
+      localStorage.removeItem('超大弹幕处理方式')
+    }
+  }
 })();
