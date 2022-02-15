7c375e4c7ed460426fede98a6e61bb46d90dd4e0
shaw <haseeer@gmail.com>
2022-02-15 22:49:39 +0800

    尝试解决异步问题

--- src/autoBnana.js
+++ src/autoBnana.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         acfun自动投蕉
 // @namespace    http://tampermonkey.net/
-// @version      0.1.3
+// @version      0.1.4
 // @description  已关注up主的视频、文章自动投5蕉，无任何依赖，原生操作
 // @author       泥壕
 // @match        https://www.acfun.cn/v/*
@@ -26,7 +26,11 @@
     document.querySelector(`.${className}[data-num="5"]`).click()
     console.log('= auto banana done =');
   }
-  ready(() => {
+
+  /**
+   * 判断页面类型
+   */
+  function pageCheck () {
     if (location.href.includes('/v/')) {
       // 视频页面
       const staffArea = document.querySelector('.staff-area')
@@ -50,17 +54,27 @@
       bananaEat('banana')
     } else {
       // 文章页面
-      setTimeout(() => {
-        if (document.querySelector('.up-operate .focus').textContent === '关注') {
-          // 未关注
-          return
-        }
-        if (document.querySelector('#article-operation .bananacount').className.includes('active')) {
-          // 已投蕉
-          return
-        }
-        bananaEat('bananaer')
-      }, 3000)
+      if (document.querySelector('.up-operate .focus').textContent === '关注') {
+        // 未关注
+        return
+      }
+      if (document.querySelector('#article-operation .bananacount').className.includes('active')) {
+        // 已投蕉
+        return
+      }
+      bananaEat('bananaer')
     }
-  })
+  }
+  ready(() => {
+    setTimeout(() => {
+      pageCheck()
+      setTimeout(() => {
+        document.querySelector('.recmd').addEventListener('click', () => {
+          setTimeout(() => {
+            pageCheck()
+          }, 3000)
+        })
+      }, 3000)
+    })
+  }, 3000)
 })();
