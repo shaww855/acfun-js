39c148e6e9ba6892a95f55c98bd019b1873a1bc2
shaw <haseeer@gmail.com>
2022-02-20 16:33:49 +0800

    acfun牌子名称长度限制

---
+++ src/medalName.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         acfun牌子名称长度限制
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  acfun牌子名称长度限制，防止外语文本溢出导致阅读困难
+// @author       泥壕
+// @match        https://live.acfun.cn/live/*
+// @icon         https://cdn.aixifan.com/ico/favicon.ico
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function ready (fn) {
+    if (document.readyState != 'loading') {
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+  console.log('ready');
+
+  ready(() => {
+    const inject = document.createElement('style')
+    inject.innerHTML = `
+    .medal-wrapper .medal-name {
+        max-width:40px;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+    `
+    document.querySelector('head').appendChild(inject)
+    console.log('inject', inject);
+  })
+})();
